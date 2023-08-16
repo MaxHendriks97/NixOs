510d8f0c95cca98e48f6eac0ef58175e5a331f5e
MaxIdgis <max.hendriks@idgis.nl>
2023-08-16 08:13:55 +0200

    Hyprland colorscheme adjustment

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -76,8 +76,8 @@
           gaps_in = 5
           gaps_out = 20
           border_size = 2
-          col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
-          col.inactive_border = rgba(595959aa)
+          col.active_border = rgb(${config.colorScheme.colors.base0C}) rgb(${config.colorScheme.colors.base0B}) 45deg
+          col.inactive_border = rgb(${config.colorScheme.colors.base04})
 
           layout = dwindle
       }
