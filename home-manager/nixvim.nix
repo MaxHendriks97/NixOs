fccbfd714fa7b4efca510b28b239ebcb34b3ce7e
Max Hendriks <max.hendriks1712@gmail.com>
2023-08-19 16:11:40 +0200

    NixVim setup

--- home-manager/nixvim.nix
+++ home-manager/nixvim.nix
@@ -32,6 +32,59 @@
         action = "<CMD>qa<CR>";
         desc = "quit";
       };
+      "<leader>b" = {
+        desc = "Buffer";
+      };
+      "<leader>bk" = {
+        action = "<CMD>bd<CR>";
+        desc = "Kill buffer";
+      };
+      "<leader>bj" = {
+        action = "<CMD>BufferLinePick<CR>";
+        desc = "Jump to buffer";
+      };
+      "gt" = {
+        action = "<CMD>bn<CR>";
+      };
+      "gT" = {
+        action = "<CMD>bp<CR>";
+      };
+      "<leader>w" = {
+        desc = "Window";
+      };
+      "<leader>ws" = {
+        action = "<CMD>vs<CR>";
+        desc = "Vertical split";
+      };
+      "<leader>wk" = {
+        action = "<CMD>q<CR>";
+        desc = "Kill window";
+      };
+      "<leader>w<up>" = {
+        action = "<CMD>wincmd k<CR>";
+      };
+      "<leader>w<down>" = {
+        action = "<CMD>wincmd j<CR>";
+      };
+      "<leader>w<left>" = {
+        action = "<CMD>wincmd h<CR>";
+      };
+      "<leader>w<right>" = {
+        action = "<CMD>wincmd l<CR>";
+      };
+      "<leader>o" = {
+        desc = "open";
+      };
+      "<leader>op" = {
+        action = "<CMD>CHADopen<CR>";
+      };
+      "<leader>g" = {
+        desc = "git";
+      };
+      "<leader>gg" = {
+        action = "<CMD>LazyGit<CR>";
+        desc = "Open LazyGit";
+      };
     };
     colorschemes.catppuccin = {
       enable = true;
@@ -41,6 +94,7 @@
     options = {
       number = true;
       relativenumber = true;
+      termguicolors = true;
     };
     plugins = {
       which-key = {
@@ -53,7 +107,8 @@
       };
       copilot-lua = {
         enable = true;
-        suggestion = {
+        suggestion.enabled = true;
+        panel = {
           enabled = true;
           autoTrigger = true;
         };
@@ -85,6 +140,27 @@
           { name = "path"; }
           { name = "buffer"; }
         ];
+        mapping = {
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<Tab>" = {
+            modes = [ "i" "s" ];
+            action = ''
+              function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif luasnip.expandable() then
+                  luasnip.expand()
+                elseif luasnip.expand_or_jumpable() then
+                  luasnip.expand_or_jump()
+                elseif check_backspace() then
+                  fallback()
+                else
+                  fallback()
+                end
+              end
+            '';
+          };
+        };
       };
       lsp = {
         enable = true;
@@ -182,6 +258,21 @@
           project-nvim.enable = true;
         };
       };
+      leap.enable = true;
+      bufferline = {
+        enable = true;
+        showCloseIcon = false;
+      };
+      auto-session.enable = true;
+      chadtree.enable = true;
+      comment-nvim.enable = true;
+      toggleterm = {
+        enable = true;
+        openMapping = "<leader>ot";
+      };
     };
+    extraPlugins = [
+      pkgs.vimPlugins.lazygit-nvim
+    ];
   };
 }
