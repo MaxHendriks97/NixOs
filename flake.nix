f27c9a84f9288b7060605eb2a7ea384abe3181cf
MaxIdgis <max.hendriks@idgis.nl>
2023-09-25 07:59:01 +0200

    Set home-manager to correct version

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     emacs.url = "github:nix-community/emacs-overlay";
     nixvim.url = "github:nix-community/nixvim";
