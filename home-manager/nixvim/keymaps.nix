a20d85e85aeb803c9d08940deaf2e5c66b00f0e9
Max Hendriks <max.hendriks1712@gmail.com>
2023-10-03 14:45:06 +0200

    Transition to new keymap system

--- home-manager/nixvim/keymaps.nix
+++ home-manager/nixvim/keymaps.nix
@@ -3,128 +3,42 @@
     mapleader = " ";
     maplocalleader = " ";
   };
-  programs.nixvim.maps = {
-    terminal = {
-      "<ESC>" = {
-        action = "<C-\\><C-n>";
-      };
-    };
-    normalVisualOp = {
-      "w".action = "<CMD>lua require('spider').motion('w')<CR>";
-      "e".action = "<CMD>lua require('spider').motion('e')<CR>";
-      "b".action = "<CMD>lua require('spider').motion('b')<CR>";
-      "ge".action = "<CMD>lua require('spider').motion('ge')<CR>";
-    };
-    normal = {
-      " " = {
-        action = "<Nop>";
-        silent = true;
-      };
-      "<leader>f" = {
-        desc = "File";
-      };
-      "<leader>ff" = {
-        action = "<CMD>Telescope find_files<CR>";
-        desc = "Find File";
-      };
-      "<leader>fs" = {
-        action = "<CMD>w<CR>";
-        desc = "Save";
-      };
-      "<leader>q" = {
-        desc = "quit/session";
-      };
-      "<leader>qq" = {
-        action = "<CMD>qa<CR>";
-        desc = "quit";
-      };
-      "<leader>b" = {
-        desc = "Buffer";
-      };
-      "<leader>bk" = {
-        action = "<CMD>lua MiniBufremove.delete(0)<CR>";
-        desc = "Kill buffer";
-      };
-      "<leader>bj" = {
-        action = "<CMD>BufferLinePick<CR>";
-        desc = "Jump to buffer";
-      };
-      "gt" = {
-        action = "<CMD>bn<CR>";
-      };
-      "gT" = {
-        action = "<CMD>bp<CR>";
-      };
-      "<leader>w" = {
-        desc = "Window";
-      };
-      "<leader>ws" = {
-        action = "<CMD>vs<CR>";
-        desc = "Vertical split";
-      };
-      "<leader>wc" = {
-        action = "<CMD>q<CR>";
-        desc = "Close window";
-      };
-      "<leader>w<up>" = {
-        action = "<CMD>wincmd k<CR>";
-      };
-      "<leader>w<down>" = {
-        action = "<CMD>wincmd j<CR>";
-      };
-      "<leader>w<left>" = {
-        action = "<CMD>wincmd h<CR>";
-      };
-      "<leader>w<right>" = {
-        action = "<CMD>wincmd l<CR>";
-      };
-      "<leader>wv" = {
-        desc = "Vertical split";
-        action = "<CMD>vsplit<CR>";
-      };
-      "<leader>wh" = {
-        desc = "Horizontal split";
-        action = "<CMD>split<CR>";
-      };
-      "<leader>o" = {
-        desc = "open";
-      };
-      "<leader>op" = {
-        action = "<CMD>CHADopen<CR>";
-      };
-      "<leader>of" = {
-        action = "<CMD>lua MiniFiles.open()<CR>";
-        desc = "Open Minifiles";
-      };
-      "<leader>g" = {
-        desc = "git";
-      };
-      "<leader>gg" = {
-        action = "<CMD>Neogit<CR>";
-        desc = "Open Neogit";
-      };
-      "<leader>p" = {
-        desc = "Project";
-      };
-      "<leader>pp" = {
-        action = "<CMD>Telescope projects<CR>";
-        desc = "Select Project";
-      };
-      "<leader>pd" = {
-        action = "<CMD>TroubleToggle workspace_diagnostics<CR>";
-        desc = "Diagnostics";
-      };
-      "<leader>pf" = {
-        action = "<CMD>Telescope live_grep<CR>";
-        desc = "Find string in project";
-      };
-      "<leader>c" = {
-        desc = "Code";
-      };
-      "<leader>cd" = {
-        action = "<CMD>TroubleToggle document_diagnostics<CR>";
-        desc = "Diagnostics";
-      };
-    };
-  };
+  programs.nixvim.keymaps = [
+    { key = "<ESC>"; action = "<C-\\><C-n>"; mode = "t"; }
+    { key = "w"; action = "<CMD>lua require('spider').motion('w')<CR>"; }
+    { key = "e"; action = "<CMD>lua require('spider').motion('e')<CR>"; }
+    { key = "b"; action = "<CMD>lua require('spider').motion('b')<CR>"; }
+    { key = "ge"; action = "<CMD>lua require('spider').motion('ge')<CR>"; }
+    { key = " "; action = "<Nop>"; options.silent = true; }
+    #{ key = "<leader>f"; options.desc = "File"; }
+    { key = "<leader>ff"; action = "<CMD>Telescope find_files<CR>"; options.desc = "Find File"; }
+    { key = "<leader>fs"; action = "<CMD>w<CR>"; options.desc = "Save"; }
+    ##{ key = "<leader>q"; options.desc = "quit/session"; }
+    { key = "<leader>qq"; action = "<CMD>qa<CR>"; options.desc = "quit"; }
+    ##{ key = "<leader>b"; options.desc = "Buffer"; }
+    { key = "<leader>bk"; action = "<CMD>lua MiniBufremove.delete(0)<CR>"; options.desc = "Kill buffer"; }
+    { key = "<leader>bj"; action = "<CMD>BufferLinePick<CR>"; options.desc = "Jump to buffer"; }
+    { key = "gt"; action = "<CMD>bn<CR>"; }
+    { key = "gT"; action = "<CMD>bp<CR>"; }
+    #{ key = "<leader>w"; options.desc = "Window"; }
+    { key = "<leader>ws"; action = "<CMD>vs<CR>"; options.desc = "Vertical split"; }
+    { key = "<leader>wc"; action = "<CMD>q<CR>"; options.desc = "Close window"; }
+    { key = "<leader>w<up>"; action = "<CMD>wincmd k<CR>"; }
+    { key = "<leader>w<down>"; action = "<CMD>wincmd j<CR>"; }
+    { key = "<leader>w<left>"; action = "<CMD>wincmd h<CR>"; }
+    { key = "<leader>w<right>"; action = "<CMD>wincmd l<CR>"; }
+    { key = "<leader>wv"; action = "<CMD>vsplit<CR>"; options.desc = "Vertical split"; }
+    { key = "<leader>wh"; action = "<CMD>split<CR>"; options.desc = "Horizontal split"; }
+    #{ key = "<leader>o"; options.desc = "open"; }
+    { key = "<leader>op"; action = "<CMD>CHADopen<CR>"; }
+    { key = "<leader>of"; action = "<CMD>lua MiniFiles.open()<CR>"; options.desc = "Open Minifiles"; }
+    #{ key = "<leader>g"; options.desc = "git"; }
+    { key = "<leader>gg"; action = "<CMD>Neogit<CR>"; options.desc = "Open Neogit"; }
+    #{ key = "<leader>p"; options.desc = "Project"; }
+    { key = "<leader>pp"; action = "<CMD>Telescope projects<CR>"; options.desc = "Select Project"; }
+    { key = "<leader>pd"; action = "<CMD>TroubleToggle workspace_diagnostics<CR>"; options.desc = "Diagnostics"; }
+    { key = "<leader>pf"; action = "<CMD>Telescope live_grep<CR>"; options.desc = "Find string in project"; }
+    #{ key = "<leader>c"; options.desc = "Code"; }
+    { key = "<leader>cd"; action = "<CMD>TroubleToggle document_diagnostics<CR>"; options.desc = "Diagnostics"; }
+  ];
 }
