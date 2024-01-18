0fdf99693aab0132cb0a1983e5f94de6f156eb7b
Max Hendriks <max.hendriks@idgis.nl>
2024-01-18 07:10:37 +0100

    Install xrdb, MegaSync

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -170,10 +170,12 @@
     pkgs.cargo
     pkgs.gimp
     pkgs.libreoffice
+    pkgs.xorg.xrdb
   ];
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
+  services.megasync.enable = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.11";
