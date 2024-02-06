34055b83db355394558cb711becfeb4ee559a215
Max Hendriks <max.hendriks@idgis.nl>
2024-02-06 08:31:48 +0100

    Install MySQL database for PHP

--- nixos/common.nix
+++ nixos/common.nix
@@ -277,6 +277,10 @@
 
   services.gvfs.enable = true;
   services.tumbler.enable = true;
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
+  };
 
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
