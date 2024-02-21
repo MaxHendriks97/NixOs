8dd0df3265e67d94a22882e00c124b5759db6325
MaxIdgis <max.hendriks@idgis.nl>
2024-02-21 08:53:45 +0100

    Set rofi theme

--- home-manager/modules/rofi.nix
+++ home-manager/modules/rofi.nix
@@ -3,6 +3,7 @@
     programs.rofi = {
       enable = true;
       package = pkgs.rofi-wayland;
+      theme = "~/NixOs/remotes/rofi/deathemonic/config/launcher.rasi";
       plugins = [
         pkgs.rofimoji
       ];
