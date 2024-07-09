e26f5d76bcfaa9eabeb58f5a7fc788b6e8a3e415
Max Hendriks <max.hendriks1712@gmail.com>
2024-07-09 09:28:05 +0200

    Fix xdg portals

--- nixos/common.nix
+++ nixos/common.nix
@@ -108,8 +108,12 @@
       enable = true;
       extraPortals = [
         pkgs.xdg-desktop-portal-hyprland
+        pkgs.xdg-desktop-portal-gtk
       ];
-      config.common.default = "*";
+      config.common = {
+        default = "hyprland;gtk";
+        "org.freedesktop.portal.FileChooser" = "gtk";
+      };
     };
     mime = {
       enable = true;
