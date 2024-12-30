ae8d5a55d6b22dfca9687327555c0c21736e5a32
Max Hendriks <max@aqqo.com>
2024-12-30 08:16:10 +0100

    Add lastpass-cli, formatting

--- nixos/aqqo_dev.nix
+++ nixos/aqqo_dev.nix
@@ -1,7 +1,16 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 
-{ inputs, outputs, lib, config, pkgs, nixpkgs-openvpn, ... }: {
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  nixpkgs-openvpn,
+  ...
+}:
+{
   # You can import other NixOS modules here
   networking.hosts = {
     "127.0.0.1" = [
@@ -18,33 +27,38 @@
     ];
   };
 
-  environment.systemPackages = let 
-    myphp = pkgs.php82.buildEnv {
-      extensions = {enabled, all}:
-        enabled ++ [
-          all.curl
-          all.intl
-          all.mbstring
-          all.mysqli
-          all.opcache
-          all.xml
-          all.zip
-          all.ssh2
-        ];
-      extraConfig = "memory_limit = 5G";
-    };
-  in [
-    # Aqqo Dev tools
-    myphp
-    myphp.packages.composer
-    pkgs.yarn
-    pkgs.flyway
-    pkgs.mysql-workbench
-    pkgs.gettext
-    pkgs.onedrive
-    pkgs.onedrivegui
-    pkgs.openvpn
-  ];
+  environment.systemPackages =
+    let
+      myphp = pkgs.php82.buildEnv {
+        extensions =
+          { enabled, all }:
+          enabled
+          ++ [
+            all.curl
+            all.intl
+            all.mbstring
+            all.mysqli
+            all.opcache
+            all.xml
+            all.zip
+            all.ssh2
+          ];
+        extraConfig = "memory_limit = 5G";
+      };
+    in
+    [
+      # Aqqo Dev tools
+      myphp
+      myphp.packages.composer
+      pkgs.yarn
+      pkgs.flyway
+      pkgs.mysql-workbench
+      pkgs.gettext
+      pkgs.onedrive
+      pkgs.onedrivegui
+      pkgs.openvpn
+      pkgs.lastpass-cli
+    ];
 
   programs.thunderbird = {
     enable = true;
