bd1cadd163dfa6a23a96be2448a55bd807676f2c
Max Hendriks <max@aqqo.com>
2025-01-16 11:15:02 +0100

    Formatting, disable phpstan

--- home-manager/modules/nixvim/lsp.nix
+++ home-manager/modules/nixvim/lsp.nix
@@ -1,53 +1,54 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   programs.nixvim.plugins = {
-      lsp = {
-        enable = true;
-        keymaps.lspBuf = {
-          "gd" = "definition";
-          "gD" = "references";
-          "gi" = "implementation";
-          "<leader>ca" = "code_action";
-        };
-        servers = {
-          nixd.enable = true;
-          ts_ls.enable = true;
-          intelephense = {
-            enable = true;
-            package = pkgs.intelephense;
-          };
-          eslint.enable = true;
-        };
+    lsp = {
+      enable = true;
+      keymaps.lspBuf = {
+        "gd" = "definition";
+        "gD" = "references";
+        "gi" = "implementation";
+        "<leader>ca" = "code_action";
       };
-      none-ls = {
-        enable = true;
-        sources = {
-          diagnostics = {
-            phpstan = {
-              enable = true;
-            };
-          };
-        };
-      };
-      lsp-format = {
-        enable = true;
-        lspServersToEnable = [
-          "nixd"
-          "ts_ls"
-          "eslint"
-        ];
-      };
-      lspkind = {
-        enable = true;
-        cmp = {
+      servers = {
+        nixd.enable = true;
+        ts_ls.enable = true;
+        intelephense = {
           enable = true;
-          menu = {
-            nvim_lsp = "[LSP]";
-            treesitter = "[TS]";
-            nvim_lua = "[LUA]";
-            buffer = "[BUF]";
-            path = "[PATH]";
+          package = pkgs.intelephense;
+        };
+        eslint.enable = true;
+      };
+    };
+    none-ls = {
+      enable = true;
+      sources = {
+        diagnostics = {
+          phpstan = {
+            enable = false;
           };
         };
       };
+    };
+    lsp-format = {
+      enable = true;
+      lspServersToEnable = [
+        "nixd"
+        "ts_ls"
+        "eslint"
+      ];
+    };
+    lspkind = {
+      enable = true;
+      cmp = {
+        enable = true;
+        menu = {
+          nvim_lsp = "[LSP]";
+          treesitter = "[TS]";
+          nvim_lua = "[LUA]";
+          buffer = "[BUF]";
+          path = "[PATH]";
+        };
+      };
+    };
   };
 }
